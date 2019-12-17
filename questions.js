82d951053a570e7740c5fee77a1274534ed8a7df
LPMartinez <lpmartinez1109@gmail.com>
2019-12-17 09:17:37 -0600

    Added var questions and five questions to script file

---
+++ questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "Which of the following is not a Chicago sports team?",
+        choices: ["Bears", "Wolves", "Sky", "Cardinals"],
+        answer: "Cardinals"
+    },
+    {
+        title: "What is the name of the player known as 'Sweetness'?",
+        choices: ["Devin Hester", "Walter Payton", "William Perry", "Gale Sayers"],
+        answer: "Walter Payton"
+    },
+    {
+        title: "What year did the Cubs finally end their championship drought?",
+        choices: ["2015", "2010", "2019", "2016"],
+        answer: "2016"
+    }
+    {
+        title: "How many Stanley cups have Chicago Blackhawks won?",
+        choices: ["6", "4", "2", "16"],
+        answer: "6"
+    }
+    {
+        title: "Which of the following is not one of Michael Jordan's nicknames?",
+        choices: ["His Airness", "MJ", "Air Jordan", "Magic Jordan"],
+        answer: "Magic Jordan"
+    }
+]
