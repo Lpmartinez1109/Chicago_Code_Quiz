9ce7a7a3a039e40bb0d085024c3fa8900c4cd8e2
LPMartinez <lpmartinez1109@gmail.com>
2020-03-10 21:39:03 -0500

    Files created to fix code quiz

---
+++ logic.js
@@ -0,0 +1,24 @@
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerId;
+
+var questionsEl = document.getElementById("questions");
+var timerEl = document.getElementById("time");
+var choicesEl = document.getElementById("choices");
+var submitBtn = document.getElementById("submit");
+var startBtn = document.getElementById("start");
+var initialsEl = document.getElementById("initials");
+var feedbackEl = document.getElementById("feedback");
+
+function startQuiz(){
+    var startScreenEl = document.getElementById("start-screen");
+    startScreenEl.setAttribute("class", "hide");
+
+    questionsEl.removeAttribute("class");
+
+    timerId = setInterval(clockTick, 1000);
+
+    timerEl.textContent = time;
+
+    getQuestion();
+}
